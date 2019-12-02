7b63f53f49bee6287c3670c1643a9f5f3c085344
AmaranthineCodices <git@amaranthinecodices.me>
2019-12-01 23:10:23 -0800

    day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -2,8 +2,10 @@ fn get_fuel_cost(mass: usize) -> usize {
     (mass / 3) - 2
 }
 
-fn get_total_fuel_cost(masses: Vec<usize>) -> usize {
-    masses.iter().map(|m| get_fuel_cost(*m)).sum()
+fn requires_fuel_addition(mass: usize) -> bool {
+    // We need to add some amount of fuel for a mass if, when divided by three,
+    // it's greater than 2 still.
+    mass / 3 > 2
 }
 
 pub struct DayOne;
@@ -14,7 +16,7 @@ impl crate::PuzzleSolver for DayOne {
     }
 
     fn solve(&self, input: &str) {
-        let masses = input
+        let masses: Vec<usize> = input
             .lines()
             .map(|l| {
                 l.parse::<usize>()
@@ -22,7 +24,26 @@ impl crate::PuzzleSolver for DayOne {
             })
             .collect();
 
-        let total_fuel_cost = get_total_fuel_cost(masses);
-        println!("Part one (total fuel cost): {}", total_fuel_cost);
+        let module_fuel_costs: Vec<usize> = masses.iter().map(|m| get_fuel_cost(*m)).collect();
+
+        let module_fuel_cost: usize = module_fuel_costs.iter().sum();
+        println!("Part one (module fuel cost): {}", module_fuel_cost);
+
+        let finalized_fuel_cost: usize = module_fuel_costs
+            .iter()
+            .map(|f| {
+                let mut last_fuel = *f;
+                let mut accumulator: usize = 0;
+                while requires_fuel_addition(last_fuel) {
+                    let new_fuel = get_fuel_cost(last_fuel);
+                    accumulator = accumulator + new_fuel;
+                    last_fuel = new_fuel;
+                }
+
+                f + accumulator
+            })
+            .sum();
+
+        println!("Part two (total fuel cost): {}", finalized_fuel_cost)
     }
 }
