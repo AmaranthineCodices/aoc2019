8f28104fb0c96447833ff850b494fd4d3dc0e15f
AmaranthineCodices <git@amaranthinecodices.me>
2019-12-03 23:02:45 -0800

    day 4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -16,6 +16,38 @@ fn has_consecutive_digits(number: usize) -> bool {
     false
 }
 
+fn has_exactly_two_consecutive_digits(number: usize) -> bool {
+    let mut digits = vec![number % 10];
+    let mut remainder = number / 10;
+
+    while remainder > 0 {
+        let current_digit = remainder % 10;
+        digits.insert(0, current_digit);
+        remainder = remainder / 10;
+    }
+
+    let mut index = 0;
+
+    while index <= 5 {
+        let digit = digits[index];
+        let mut consecutive_count = 0;
+
+        for remaining_index in index..6 {
+            if digits[remaining_index] == digit {
+                consecutive_count += 1;
+            }
+        }
+
+        if consecutive_count == 2 {
+            return true;
+        }
+
+        index += consecutive_count;
+    }
+
+    false
+}
+
 fn do_digits_ascend(number: usize) -> bool {
     let mut last_digit = number % 10;
     let mut remainder = number / 10;
@@ -68,6 +100,13 @@ impl crate::PuzzleSolver for DayFour {
         }
 
         println!("Part 1: {} possible passwords", possible_passwords.len());
+
+        let part_two = possible_passwords
+            .iter()
+            .filter(|p| has_exactly_two_consecutive_digits(**p))
+            .count();
+
+        println!("Part 2: {} possible passwords under new rules", part_two);
     }
 }
 
@@ -86,5 +125,16 @@ mod tests {
     fn test_do_digits_ascend() {
         assert!(do_digits_ascend(123456));
         assert!(!do_digits_ascend(123245));
+    }
+
+    #[test]
+    fn test_has_exactly_two_consecutive_digits() {
+        assert!(has_exactly_two_consecutive_digits(112345));
+        assert!(!has_exactly_two_consecutive_digits(111234));
+        assert!(has_exactly_two_consecutive_digits(123345));
+        assert!(has_exactly_two_consecutive_digits(123455));
+        assert!(!has_exactly_two_consecutive_digits(123334));
+        assert!(!has_exactly_two_consecutive_digits(123444));
+        assert!(has_exactly_two_consecutive_digits(111122));
     }
 }
