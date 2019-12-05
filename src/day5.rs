1c59c0fe322d94a8f27d9462278e095ebabcce3a
AmaranthineCodices <git@amaranthinecodices.me>
2019-12-04 21:54:13 -0800

    add verification tests (for part 2's instruction set)

--- src/day5.rs
+++ src/day5.rs
@@ -202,12 +202,85 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_interpreter() {
+    fn test_input_eq_eight_position() {
         let source_program = vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8];
         let output = run_interpreter(&source_program, 8);
         assert_eq!(output, vec![1]);
 
         let output = run_interpreter(&source_program, 1);
         assert_eq!(output, vec![0]);
+    }
+
+    #[test]
+    fn test_input_lt_eight_position() {
+        let source_program = vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8];
+        let output = run_interpreter(&source_program, 8);
+        assert_eq!(output, vec![0]);
+
+        let output = run_interpreter(&source_program, 1);
+        assert_eq!(output, vec![1]);
+
+        let output = run_interpreter(&source_program, 10);
+        assert_eq!(output, vec![0]);
+    }
+
+    #[test]
+    fn test_input_eq_eight_immediate() {
+        let source_program = vec![3, 3, 1108, -1, 8, 3, 4, 3, 99];
+        let output = run_interpreter(&source_program, 8);
+        assert_eq!(output, vec![1]);
+
+        let output = run_interpreter(&source_program, 1);
+        assert_eq!(output, vec![0]);
+    }
+
+    #[test]
+    fn test_input_lt_eight_immediate() {
+        let source_program = vec![3, 3, 1107, -1, 8, 3, 4, 3, 99];
+        let output = run_interpreter(&source_program, 8);
+        assert_eq!(output, vec![0]);
+
+        let output = run_interpreter(&source_program, 1);
+        assert_eq!(output, vec![1]);
+
+        let output = run_interpreter(&source_program, 10);
+        assert_eq!(output, vec![0]);
+    }
+
+    #[test]
+    fn test_input_non_zero_position() {
+        let source_program = vec![3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9];
+        let output = run_interpreter(&source_program, 8);
+        assert_eq!(output, vec![1]);
+
+        let output = run_interpreter(&source_program, 0);
+        assert_eq!(output, vec![0]);
+    }
+
+    #[test]
+    fn test_input_non_zero_immediate() {
+        let source_program = vec![3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1];
+        let output = run_interpreter(&source_program, 8);
+        assert_eq!(output, vec![1]);
+
+        let output = run_interpreter(&source_program, 0);
+        assert_eq!(output, vec![0]);
+    }
+
+    #[test]
+    fn test_input_value() {
+        let source_program = vec![
+            3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0,
+            0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4,
+            20, 1105, 1, 46, 98, 99,
+        ];
+        let output = run_interpreter(&source_program, 8);
+        assert_eq!(output, vec![1000]);
+
+        let output = run_interpreter(&source_program, 0);
+        assert_eq!(output, vec![999]);
+
+        let output = run_interpreter(&source_program, 9);
+        assert_eq!(output, vec![1001]);
     }
 }
